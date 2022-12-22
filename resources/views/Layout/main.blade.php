d98e2aee3c3bf8bec5d469c5aff837b679164a96
Chatra11 <2010817310007@mhs.ulm.ac.id>
2022-12-22 14:31:02 +0800

    ganti nama folder

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Sistem Informasi Apotek</title>
+        <title>Sistem Informasi Apotek </title>
         <!-- Favicon-->
         <!-- Core theme CSS (includes Bootstrap)-->
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
