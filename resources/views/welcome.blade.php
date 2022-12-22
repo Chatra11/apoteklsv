67c2610cef8993d33b10117e5e484c08455a2f24
Chatra11 <2010817310007@mhs.ulm.ac.id>
2022-12-22 14:42:51 +0800

    all 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.main')
+@extends('Layout.main')
 @section('content')
     <h2 style= "text-align:Center">Welcome to Sistem Informasi Apotek, {{auth()->user()->name}}</h2>
     <div class="row">
